a812f8a5ea67d5b9131f2ae93b2283296a05177b
Austin Christian Adithia <aadithia12@Austins-MacBook-Air-3.local>
2026-04-18 16:23:02 -0700

    second commit

--- app/user/[id]/page.js
+++ app/user/[id]/page.js
@@ -1,12 +1,8 @@
 // TODO 4: Create app/user/[id]/page.js
 
 // Hint: Dynamic route pages receive params as a prop
-export default async function UserProfile({ 
-    params, 
-}: {
-    params: Promise<{ id: number }>;
-}) {
+export default async function UserProfile({ params }) {
   // Access params.id here
-  const {id} = await params;
+  const { id } = await params;
   return <h1>User Profile: {id}</h1>;
 }
